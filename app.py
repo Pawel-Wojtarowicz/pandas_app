81c3e8dbcee03123fe758ff1399c5ed6291d9cfa
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-06-03 12:56:49 +0200

    Added streamlit to app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import pandas as pd
-# import plotly.express as px
-# import streamlit as st
+import plotly.express as px
+import streamlit as st
 
-# st.set_page_config(page_title="Statystyki roczników GUS",
-#                    page_icon=":chart_with_downwards_trend:", layout="wide")
+st.set_page_config(page_title="Statystyki roczników GUS",
+                   page_icon=":chart_with_downwards_trend:", layout="wide")
 
 # load file
 df = pd.read_excel(io="data.xls", sheet_name="roczniki",
@@ -36,3 +36,37 @@ def set_years():
 set_years()
 print(df)
 
+# sidebar
+st.sidebar.header("Filtruj:")
+year = st.sidebar.multiselect(
+    "Wybierz Rok:", options=df["Rok"].unique(), default=[2022])
+age = st.sidebar.multiselect(
+    "Wybierz Wiek:", options=df["Wiek"].unique(), default=df["Wiek"][21:23])
+#slider_range = st.sidebar.slider("Wybierz zakres:", value=[0, 100])
+# print(slider)
+
+df_selection = df.query("Rok == @year & Wiek == @age")
+
+
+# main page
+st.title(":bar_chart: Statystyki roczników - GUS")
+st.markdown("##")
+
+total = int(df_selection["Ogółem"].sum())
+men = int(df_selection["Mężczyźni"].sum())
+women = int(df_selection["Kobiety"].sum())
+
+left_col, middle_col, right_col = st.columns(3)
+with left_col:
+    st.subheader("Ogółem:")
+    st.subheader(total)
+with middle_col:
+    st.subheader("Mężczyźni:")
+    st.subheader(men)
+with right_col:
+    st.subheader("Kobiety:")
+    st.subheader(women)
+
+st.markdown("---")
+
+st.dataframe(df_selection)
